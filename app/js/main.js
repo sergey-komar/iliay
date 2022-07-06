dab6dcc97d253cb39513f1070cd5bde03a8e5613
sergey-komar <kursev81@yandex.ru>
2022-07-07 00:03:05 +0300

    project

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,31 @@
 $(function () {
+$('.slider-block__inner').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    prevArrow: '<button type="button" class="slick-prev-rigth"></button>',
+    nextArrow: '<button type="button" class="slick-next-left"></button>',
+});
 
-})
+
+$('.reviews-slider__cart').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<button type="button" class="slick-reviews-left"></button>',
+    nextArrow: '<button type="button" class="slick-reviews-rigth"></button>',
+ 
+});
+
+
+// $('.play-block__popup-link').magnificPopup({
+//     disableOn: 700,
+//     type: 'iframe',
+//     mainClass: 'mfp-fade',
+//     removalDelay: 160,
+//     preloader: false,
+
+//     fixedContentPos: false
+// });
+
+
+
+});
